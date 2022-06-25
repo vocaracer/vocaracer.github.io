1197c10ed47b95de625202c84245432600cb453a
vocaracer <108160323+vocaracer@users.noreply.github.com>
2022-06-24 17:44:02 -0700

    Create vocab.js

---
+++ vocab.js
@@ -0,0 +1,74 @@
+let vocab = {
+    "1 - 1": {
+        "A story with characters and events that are symbols representing truths about human life": "allegory",
+        " A passing or casual reference; an incidental mention of something": "allusion",
+        "A comparison of two diffferent things that are similar in some way": "analogy",
+        "A short account of a particular incident or event": "anecdote",
+        "\"A principal character or force in opposition to a protagonist, or main character\"": "antagonist",
+        "The word(s) to which a pronoun refers.": "antecedents",
+        "The exact opposite": "antithesis",
+        "A word having a meaning opposite to that of another word": "antonym",
+        "A mark ' used to show that letters or figures are missing": "apostrophe",
+        "To set or try to find the importance or value of; evaluate; estimate.": "assess"
+    },
+    "1 - 2": {
+        "\"A personal judgment either for or against a particular person, position, or thing\"": "bias",
+        "Events are presented in the order in which they occur": "chronological",
+        "An overly used expression or idea": "clich\u00e9",
+        "Logical and clear": "coherent",
+        "\"To lump, hold, or stick together\"": "cohesion",
+        "A work environment where responsibility and authority is shared equally by colleagues": "collegial",
+        "A conversational expression": "colloquialism",
+        "To understand or grasp the meaning of": "comprehend",
+        "A meaning suggested by a word or an expression in addition to its exact meaning": "connotative",
+        "The setting of a word or phrase that affects its meaning": "context"
+    },
+    "1 - 3": {
+        "An examination of how two or more things are different": "contrast",
+        "To communicate; express": "convey",
+        "Believable or plausible": "credible",
+        "To describe or portray in precise or vivid detail": "delineate",
+        "A term or lable that indicates something": "denotation",
+        "To turn away from a direct course or one that has already been set": "deviate",
+        "A mark or feature that makes someone or something different": "distinction",
+        "The leaving out of one or more words that are not necessary for a phrase to be understood": "ellipsis",
+        "To speak or pronounce in a clear voice": "enunciate",
+        "The history of a word shown by tracing it or its parts back to the earliest known forms": "etymology"
+    },
+    "2 - 1": {
+        "Verbal exchange between people with opposite views": "argument",
+        "\"Things that are normal, ordinary, and following the accepted way\"": "conventions",
+        "\"To spread information, knowledge, opinions widely\"": "disseminate",
+        "To twist out of shape; change the way a thing looks or acts": "distorted",
+        "To become known or clear": "emerge",
+        "\"To call forth or bring out (image, memory, response) in the mind or in action\"": "evoke",
+        "\"A passage or segment taken from a text\u2026phrase, sentence, paragraph, or chapter\"": "excerpt",
+        "Said or written in a clear and direct way": "explicit",
+        "Writing or speech primarily intended to convey information or to explain; a detailed statement or explanation": "exposition",
+        "Based on unsound logic; in error; illogical": "fallacious"
+    },
+    "2 - 2": {
+        "A writer\u2019s use of hints or clues to suggest events that will occur later in the plot": "foreshadowing",
+        "A figure of speech in which a statement is exaggerated for emphasis or for humorous effect": "hyperbole",
+        "A prediction or educated guess": "hypothesis",
+        "To include as part of a larger thing; blend": "incorporate",
+        "A conclusion reached on the basis of evidence and reasoning": "inference",
+        "\"The simple form of a verb that has no subject and does not show past, present or future tens\"": "infinitive",
+        "Having total knowledge": "omniscient",
+        "\"The use of words whose sounds suggest their meaning\u2026meow, buzz, splash\"": "onomatopoeia",
+        "\"A combination of contradictory words, such as \"\"tiny giant\"\"\"": "oxymoron",
+        "A statement that seems to go against common sense but may still be true": "paradox"
+    },
+    "2 - 3": {
+        "\"A form of a verb that may function as an adjective, a noun, a preposition, or part of a verb\"": "participial",
+        "The treatment of inanimate objects as if they had human feelings": "pathetic-fallacy",
+        "A position from which something is considered or evaluated; standpoint": "perspective",
+        "A word that shows a connection or relation between a noun or pronoun and some other word": "prepositional",
+        "\"Adept or skilled, usually as a result of study or practice\"": "proficient",
+        "\"To cause to move forward; thrust, push, or drive\"": "propel",
+        "The main character in fiction or drama\u2026the person who sets the plot in motion": "protagonist",
+        "To arouse or bring out": "provoke",
+        "Play on multiple meanings of a word or on two words that sound alike but have different meanings": "pun",
+        "To speak the words of from memory and in front of others": "recite"
+    }
+}
